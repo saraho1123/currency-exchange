17768043f4bd29b57a2fa8ad38bd84dcc363f25b
Sarah Osgood <sarahjs117@gmail.com>
2021-01-13 11:45:02 -0700

    initial set up for App

---
+++ src/App/App.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react'
+import { Route } from 'react-router-dom'
+import './App.scss';
+
+const App = () => {
+  return (
+    <h1>This is my app</h1>
+  )
+}
+
+export default App;
