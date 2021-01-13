cf18d2090096431db688f4e96c22ec6cc227703d
Sarah Osgood <sarahjs117@gmail.com>
2021-01-13 16:21:31 -0700

    Initial set up of App component

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,46 @@
-import React, { useState, useEffect } from 'react'
-import { Route } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Route, Switch } from 'react-router-dom'
 import './App.scss';
+import Form from '../Form/Form.js'
+import ExchangeContainer from '../ExchangeContainer/ExchangeContainer.js'
 
 const App = () => {
+  const [currencyCards, setCurrencyCards] = useState([])
+
   return (
-    <h1>This is my app</h1>
+    <main>
+      <section>
+      <h1>Currency Exchange</h1>
+      <Switch>
+        <Route 
+           
+          path='/currency-cards'
+          component={ ExchangeContainer }
+          // render={() => {
+          //   <ExchangeContainer currencyCards={currencyCards}/>
+          // }}
+        />
+        <Route
+        exact
+        path='/'
+        render={() => {
+          return (
+            <Form 
+              addCurrencyCard={addCurrencyCard} 
+              currencyCards={currencyCards} 
+              deleteCurrencyCard={deleteCurrencyCard}
+            />
+          )
+        }}
+        />
+      </Switch>
+    </section>
+      <footer>Curreny Rates for the day take from: [put the api here!]</footer>
+    </main>
   )
 }
+/*
+App shouldn't do much other than render the Form
+*/
 
 export default App;
