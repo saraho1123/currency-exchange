13496c8b199326622f99673408ff2d20478e812e
Sarah Osgood <sarahjs117@gmail.com>
2021-01-14 19:28:50 -0700

    Set up file for fetches

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,7 @@
-export const getExchangeRates = (userCurrency, newCurrency) => {
-  return fetch(`https://api.exchangeratesapi.io/latest?symbols=${userCurrency},${newCurrency}`)
-  .then(response => response.json())
+export const getExchangeRates = async (userCurrency, newCurrency) => {
+  const response = await fetch(`https://api.exchangeratesapi.io/latest?symbols=${userCurrency},${newCurrency}`)
+  const data = await response.json()
+  return data
+  // return fetch(`https://api.exchangeratesapi.io/latest?symbols=${userCurrency},${newCurrency}`)
+  //   .then(response => response.json())
 }
