d790ba9031a1bdf97ff81977691f891d3dff08bd
Sarah Osgood <sarahjs117@gmail.com>
2021-01-17 14:07:58 -0700

    Complete functionality for drop down fields for currency inputs

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
+import { currencyCodes } from '../sampleApiData.js'
 import './Form.scss'
 
 const Form = (props) => {
@@ -24,43 +25,87 @@ const Form = (props) => {
     setNewCurrency('')
   }
 
-  const toInputUppercase = event => {
-    event.target.value = ("" + event.target.value).toUpperCase();
-  };
-  
-  return (
-    <section >
+  if(!userCurrency || !userAmount || !newCurrency) {
+    return (
+      <section >
       <NavLink to='/currency-cards' className='nav'>See All Your Conversions</NavLink>
       <form className='user-input'>
-        <input 
-          type='text'
+        <select 
           className='user-currency'
-          placeholder='Type country of your currency'
-          onInput={toInputUppercase}
+          type='text'
           value={userCurrency}
-          onChange={(event) => setUserCurrency(event.target.value)}
-        />
+          onChange={(event) => setUserCurrency(event.target.value)}>  
+          {currencyCodes.map(code => {
+            return (
+              <option value={code}>{code}</option>
+              )
+            })}
+        </select>
         <input           
         type='number'
-          min='1'
-          max='10000'
-          className='user-amount'
-          placeholder='Amount to exchange (up to 10,000)'
-          value={userAmount || 1 }
-          onChange={(event) => setUserAmount(event.target.value)}
+        min='1'
+        max='10000'
+        className='user-amount'
+        placeholder='Amount to exchange (up to 10,000)'
+        value={userAmount}
+        onChange={(event) => setUserAmount(event.target.value)}
         />
-        <input 
-          type='text'
+        <select 
           className='new-currency'
-          placeholder='Type country of currency you want'
-          onInput={toInputUppercase}
+          type='text'
           value={newCurrency}
-          onChange={(event) => setNewCurrency(event.target.value)}
-        />
-      <NavLink onClick={getUserData} to="/currency-cards" className="nav">Get Currency Conversion </NavLink>
+          onChange={(event) => setNewCurrency(event.target.value)}>  
+          {currencyCodes.map(code => {
+            return (
+              <option value={code}>{code}</option>
+              )
+            })}
+        </select>
+      <NavLink to="/" className="nav">Please complete all fields</NavLink>
       </form>
     </section>
   )
+} else {
+    return (
+      <section >
+        <NavLink to='/currency-cards' className='nav'>See All Your Conversions</NavLink>
+        <form className='user-input'>
+          <select 
+            className='user-currency'
+            type='text'
+            value={userCurrency}
+            onChange={(event) => setUserCurrency(event.target.value)}>  
+            {currencyCodes.map(code => {
+              return (
+                <option value={code}>{code}</option>
+              )
+            })}
+          </select>
+          <input           
+          type='number'
+            min='1'
+            max='10000'
+            className='user-amount'
+            placeholder='Amount to exchange (up to 10,000)'
+            value={userAmount}
+            onChange={(event) => setUserAmount(event.target.value)}
+          />
+          <select 
+            className='new-currency'
+            type='text'
+            value={newCurrency}
+            onChange={(event) => setNewCurrency(event.target.value)}>  
+            {currencyCodes.map(code => {
+              return (
+                <option value={code}>{code}</option>
+              )
+            })}
+          </select>
+        <NavLink onClick={getUserData} to="/currency-cards" className="nav">Get Currency Conversion </NavLink>
+        </form>
+      </section>
+    )
+  }
 }
 
 export default Form
