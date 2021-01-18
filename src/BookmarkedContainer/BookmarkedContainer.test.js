b587bd3da6132815629d708b1ba2e76240b74b30
Sarah Osgood <sarahjs117@gmail.com>
2021-01-18 11:37:22 -0700

    Complete set up and unit tests for BookmarkedContainer

--- src/BookmarkedContainer/BookmarkedContainer.test.js
+++ src/BookmarkedContainer/BookmarkedContainer.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import BookmarkedContainer from './BookmarkedContainer'
 import { sampleApiData, multipleSampleBookmarkedCards } from '../sampleApiData.js'
 import '@testing-library/jest-dom'  // npm install --save-dev @testing-library/jest-dom
@@ -8,6 +8,7 @@ import { createMemoryHistory } from 'history'
 
 describe('Bookmarked', () => {
   const data = multipleSampleBookmarkedCards
+  const noBookmarkedConversions = []
   const removeBookmarked = jest.fn()
   const deleteCurrencyCard = jest.fn()
   const history = createMemoryHistory()
@@ -25,13 +26,43 @@ describe('Bookmarked', () => {
   })
 
   it('should render bookmarked cards', () => {
+    const phpExchangeRate = screen.getByText('48.0303530188')
+    const gbpExchangeRate = screen.getByText('0.7336110195')
+    const nzdExchangeRate = screen.getByText('1.3900527879')
 
+    expect(phpExchangeRate).toBeInTheDocument()
+    expect(gbpExchangeRate).toBeInTheDocument()
+    expect(nzdExchangeRate).toBeInTheDocument()
   })
 
   it('should show a message if there are no bookmarked cards', () => {
+    history.location.pathName='/'
+    render(
+      <Router history={history}>
+        <BookmarkedContainer 
+          bookmarkedConversions={noBookmarkedConversions}
+          deleteCurrencyCard={deleteCurrencyCard}
+          removeBookmarked={removeBookmarked}
+        />
+      </Router>
+    )
 
+    const noConversionsMessage = screen.getByText(/you have not yet bookmarked any conversions\./i)
+    expect(noConversionsMessage).toBeInTheDocument()
   })
 
-  // I think there should be some tests for the nav links
+  it('should route to the home path when Get a new Currency Exchange is clicked', async () => {
+    userEvent.click(screen.getByRole('link', {
+      name: /get a new currency exchange!/i
+    }))
+    await waitFor(() => expect(history.location.pathname).toBe('/'))
+  })
+
+  it('should route to the path "currency-cards" when See All Your Conversions is clicked', async () => {
+    userEvent.click(screen.getByRole('link', {
+      name: /see all your conversions/i
+    }))
+    await waitFor(() => expect(history.location.pathname).toBe('/currency-cards'))
+  })
 
 })
