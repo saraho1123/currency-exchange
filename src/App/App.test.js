da9fe2327f8f013bcc67b318aa5f67bab48dea3b
Sarah Osgood <sarahjs117@gmail.com>
2021-01-18 11:36:40 -0700

    Complete set up for testing for App

--- src/App/App.test.js
+++ src/App/App.test.js
@@ -1,4 +1,5 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
+import selectEvent from 'react-select-event'
 import App from './App'
 import { sampleApiData } from '../sampleApiData.js'
 import '@testing-library/jest-dom'  // npm install --save-dev @testing-library/jest-dom
@@ -24,7 +25,22 @@ describe('Bookmarked', () => {
   })
 
   it('should render the Form on page load', () => {
-    const formUserAmount = screen.getByRole('spinbutton')
+    const formUserCurreny = screen.getByTestId('user-currency-dropdown')
+    const formUserAmount = screen.getByPlaceholderText(/amount to exchange \(up to 10,000\)/i)
+    const formnewCurreny = screen.getByTestId('new-currency-dropdown')
+    expect(formUserCurreny).toBeInTheDocument()
     expect(formUserAmount).toBeInTheDocument()
+    expect(formnewCurreny).toBeInTheDocument()
   })
+
+  // it('should display a currency card when all fields are complete and the Get Currency Conversion button is clicked', async () => {
+  //   // await selectEvent.select(getByLabelText('Food'), ['Strawberry', 'Mango'])
+  //   const userCurrencyChoice = await waitFor(() => screen.getAllByText(/usd/i))
+  //   const userAmountChoice = await waitFor(() => screen.getByText(/10/i))
+  //   const newCurrencyChoice = await waitFor(() => screen.getAllByText(/php/i))
+
+  //   selectEvent.click()
+
+  //   userEvent.click(screen.getByText('Get Currency Conversion'))
+  // })
 })
